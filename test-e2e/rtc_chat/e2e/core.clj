edaeadbd0193219d2e9603938d1003aafb46a1c2
Matt Pfefferle <matthew.pfefferle@gmail.com>
2014-10-26 14:22:08 -0400

    Minimal end to end test
    It launches selenium and attempts to assert basic info about the start
    page.  It currently fails because there is no server to start.

--- test-e2e/rtc_chat/e2e/core.clj
+++ test-e2e/rtc_chat/e2e/core.clj
@@ -1,8 +1,37 @@
 (ns rtc-chat.e2e.core
-  (:require [clojure.test :refer :all])
+  (:require [clojure.test :refer :all]
+            [clj-webdriver.taxi :as t]
+            )
   )
 
-(deftest addition
-  (is (= 2 (+ 1 1)))
+(defn base-url []
+  "http://localhost:8080/")
+
+(defn start-server [f]
+  ;; app start code here
+  (f)
   )
+
+(defn start-browser-fixture
+  [f]
+  (t/set-driver! {:browser :firefox} (base-url))
+  (f))
+
+(defn reset-browser-fixture
+  [f]
+  (t/to (base-url))
+  (f))
+
+(defn quit-browser-fixture
+  [f]
+  (f)
+  (t/quit))
+
+(use-fixtures :once start-server start-browser-fixture quit-browser-fixture)
+(use-fixtures :each reset-browser-fixture)
+
+(deftest test-browser-configured
+  (is (= (class t/*driver*) clj_webdriver.driver.Driver))
+  (is (= (t/current-url) (base-url)))
+  (is (= (re-find #"(?i)html>" (t/page-source)))))
 
