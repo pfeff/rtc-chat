b997156b3c7e69f6f22704ab76caf4bc328e7172
Matt Pfefferle <matthew.pfefferle@gmail.com>
2014-11-02 20:31:23 -0500

    Integration test container
    I needed a utility to create a dom node in which I can test a react
    component.

--- test-cljs/rtc_chat/test/core.cljs
+++ test-cljs/rtc_chat/test/core.cljs
@@ -1,9 +1,31 @@
 (ns rtc-chat.test.core
   (:require [cemerick.cljs.test :as t]
+            [dommy.core :as dommy]
             [dommy.core :refer-macros [sel sel1]]
             )
   (:require-macros [cemerick.cljs.test :refer (is deftest)]))
 
-(deftest hello
-  (let [h1 (sel1 [:h1])]
-    (is (= "Hello world!" h1))))
+(defn new-id 
+  ([]
+   (str "container-" (gensym)))
+  ([id]
+   (str "container-" id)))
+
+(defn new-node [id]
+  (-> (dommy/create-element "div")
+      (dommy/set-attr! "id" id)))
+
+(defn append-node [node]
+  (dommy/append! (sel1 js/document :body) node))
+
+(defn container!
+  ([]
+   (container! (new-id)))
+  ([id]
+   (-> id
+       new-node
+       append-node)))
+
+(deftest test-container
+  (let [c (container! "container-1")]
+    (is (sel1 :#container-1))))
